0ae8d1ef998bf2d8251eb58e8de64bdc357b4a62
Joy Fan <73991926+Joy-Honchan@users.noreply.github.com>
2023-04-25 23:53:23 +0800

    Create 2626-Array-Reduce-Transformation.ts

---
+++ 2626-Array-Reduce-Transformation.ts
@@ -0,0 +1,12 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    if(nums.length === 0){ return init }
+    let result = init;
+    for(let i = 0; i < nums.length; i++){
+        result = fn(result, nums[i])
+    }
+    return result
+};
+                                   
+// forEach seems to cost more memory but less run time then for loop
